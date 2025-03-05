f3d736fdb914cd1b121c4f150891c7f6b4ffb4da
Joychema <joymelanin19@gmail.com>
2025-03-05 10:09:20 +0300

    initial commit

---
+++ myscript.js
@@ -0,0 +1,27 @@
+// const select=document.querySelector('select');
+// const list=document.querySelector('ul');
+// const h1=document.querySelector('h1');
+
+// select.addEventListener('change', () => {
+//     const choice=select.value;
+//     createCalendar(choice);
+// }) 
+// function createCalendar(month){
+//     let days =31;
+//     if (month === "February"){
+//         days=28;
+//     }else if (month === "April" || month === "June" || month === "September"){
+//         days=30;
+//     }
+// }
+// h1.textContent=month;
+// for (let i=1; i<=days, i++;) {
+
+// }
+// select.value="January";
+// createCalendar("January");
+
+function Greeting(x){
+    console.log(x);
+}
+Greeting("Hello World");
